a8846d1b6092e610eda45cb57f4792a61842e21b
Natanael Muñoz <namuji38mail.com>
2024-07-02 19:49:00 -0400

    peo

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,7 @@
 const articles = [
     {
         icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#60A5FA" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-blue-800">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#60A5FA" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-blue-100">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
         ),
@@ -11,7 +11,7 @@ const articles = [
     },
     {
         icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#60A5FA" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-blue-800">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#60A5FA" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-blue-100">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
             </svg>
 
@@ -21,7 +21,7 @@ const articles = [
     },
     {
         icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#60A5FA" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-blue-800">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#60A5FA" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-blue-100">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
         ),
@@ -30,7 +30,7 @@ const articles = [
     },
     {
         icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#60A5FA" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-blue-800">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#60A5FA" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-blue-100">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
             </svg>
         ),
