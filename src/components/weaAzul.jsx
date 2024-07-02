990076b6a4333d69f8e20b5efaf3f9e84d42dd69
Natanael Muñoz <namuji38mail.com>
2024-07-02 19:44:09 -0400

    arregle un poco la wea azul

--- src/components/weaAzul.jsx
+++ src/components/weaAzul.jsx
@@ -2,19 +2,7 @@ import articles from "@/data/data";
 
 export default function Hero() {
   return (
-    <section className="bg-blue-800 my-0 px-6 py-11 flex flex-col gap-8 md:flex-row md:gap-2 md:container md:mx-auto md:rounded-3xl md:py-5 md:px-11 md:-translate-y-14">
-
-
-      {/* {articles.map((article, index) => (
-        <article key={index} className="text-center">
-          <figure className="mx-auto mb-4">
-            {article.icon}
-          </figure>
-          <h2 className="text-lg font-semibold text-blue-600 mb-2">{article.title}</h2>
-          <p className="text-gray-700">{article.description}</p>
-        </article>
-      ))} */}
-
+    <section className="bg-blue-800 my-0 px-6 py-11 flex flex-col gap-8 md:flex-row md:gap-2 md:container md:mx-auto md:rounded-3xl md:py-5 md:px-11 md:-translate-y-11">
 
       {articles.map((articles) => (
         <div className="flex flex-row gap-2 md:w-96">
